0edda09d03eabea97ad900668e03bfaaf87a0250
dmtumilovich <dima3246@gmail.com>
2022-09-07 14:42:37 +0300

    8-search-api: initial commit

--- 6-indexing/indexing-app/indexing/src/main/java/com/einott/search/util/CsvReader.java
+++ 6-indexing/indexing-app/indexing/src/main/java/com/einott/search/util/CsvReader.java
@@ -2,6 +2,7 @@ package com.einott.search.util;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.Reader;
 import java.util.stream.Stream;
 
 import com.opencsv.bean.CsvToBean;
@@ -10,12 +11,15 @@ import com.opencsv.bean.CsvToBeanBuilder;
 public class CsvReader {
     
     public static <T> Stream<T> readCsvLazy(String csvFileName, Class<T> pojoClass) {
-        CsvToBean<T> csvToBean;
         try {
-            csvToBean = new CsvToBeanBuilder<T>(new FileReader(csvFileName)).withType(pojoClass).build();
-        } catch (Exception e) {
+            return readCsvLazy(new FileReader(csvFileName), pojoClass);
+        } catch (FileNotFoundException e) {
             throw new CsvReaderException(e);
         }
+    }
+
+    public static <T> Stream<T> readCsvLazy(Reader csvFile, Class<T> pojoClass) {
+        CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(csvFile).withType(pojoClass).build();
         return csvToBean.stream();
     }
 
---
+++ 8-search-api/search-api-app/src/main/java/com/einott/search/conf/ElasticsearchConfig.java
@@ -0,0 +1,90 @@
+package com.einott.search.conf;
+
+import javax.net.ssl.SSLContext;
+
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.http.ssl.SSLContexts;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback;
+
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Slf4j
+@Builder
+public class ElasticsearchConfig {
+
+    private String hostname;
+    private int port;
+    private String scheme;
+    private String username;
+    private String password;
+
+    public RestClient restClient() {
+        log.info("Initializing rest client for {}://{}:{}", this.scheme, this.hostname, this.port);
+        try {
+            SSLContext sslContext = SSLContexts.custom()
+                .loadTrustMaterial(null, TrustAllStrategy.INSTANCE)
+                .build();
+            CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(this.username, this.password));
+            RestClient restClient = RestClient.builder(new HttpHost(this.hostname, this.port, this.scheme))
+                .setHttpClientConfigCallback(new HttpClientConfigCallback() {
+
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder
+                            .setDefaultCredentialsProvider(credentialsProvider)
+                            .setSSLContext(sslContext)
+                            .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE); // disable SSL verification
+                    }
+
+                })
+                .build();
+            return restClient;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create rest client!", e);
+        }
+    }
+
+    public JacksonJsonpMapper jacksonJsonpMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return new JacksonJsonpMapper(objectMapper);
+    }
+
+    public static ElasticsearchConfig fromPropertiesFile(String propertiesFile) {
+        try (InputStream inputStream = ElasticsearchConfig.class.getClassLoader().getResourceAsStream(propertiesFile)) {
+
+            if (inputStream == null) {
+                throw new IllegalArgumentException("Unable to locate properties file: " + propertiesFile);
+            }
+            Properties props = new Properties();
+            props.load(inputStream);
+            return ElasticsearchConfig.builder()
+                .hostname(props.getProperty("hostname"))
+                .port(Integer.parseInt(props.getProperty("port")))
+                .scheme(props.getProperty("scheme"))
+                .username(props.getProperty("username"))
+                .password(props.getProperty("password"))
+                .build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
